8c1dfc6ae79a23128bafbc190fc2122ce2cd6fc9
YasiaKoval <94228121+YasiaKoval@users.noreply.github.com>
2021-11-22 19:03:25 +0200

    Update task1_concatenate_list.py

--- HT_02/task1_concatenate_list.py
+++ HT_02/task1_concatenate_list.py
@@ -1,6 +1,6 @@
 ranlist = [420, 'yes', 'attack helicopter', 673]
 concatenated_list = ''
-for list_el in len(ranlist):
+for list_el in range(len(ranlist)):
     list_el_1 = str(ranlist[list_el])
     concatenated_list += list_el_1
 print(concatenated_list)
