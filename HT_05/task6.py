73137aca819e5db4a1dfa6d0994e8ea145f21eba
YasiaKoval <94228121+YasiaKoval@users.noreply.github.com>
2021-12-06 23:23:17 +0200

    Add files via upload

--- HT_05/task6.py
+++ HT_05/task6.py
@@ -3,18 +3,23 @@
 # P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній:
 # https: // docs.python.org/3/library/stdtypes.html  # range
 def my_range(start, stop=None, step=1):
+
     if stop == None:
-        start, stop = 0, step
+        start, stop = 0, start
     if step == 0:
         raise ValueError
     if step > 0:
-        while True:
+        if start < stop:
             yield start
             start += step
+        else:
+            raise ValueError
     elif step < 0:
-        while True:
+        if start > stop:
             yield start
             start += step
+        else:
+            raise ValueError
 
 
 for rang_num in my_range():
