86fa38a8737618ed886ac3c14ba62fc2461b6707
YasiaKoval <94228121+YasiaKoval@users.noreply.github.com>
2021-12-06 23:34:45 +0200

    Update task6.py

--- HT_05/task6.py
+++ HT_05/task6.py
@@ -3,19 +3,20 @@
 # P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній:
 # https: // docs.python.org/3/library/stdtypes.html  # range
 def my_range(start, stop=None, step=1):
-
+    if start == 0:
+        return []
     if stop == None:
         start, stop = 0, start
     if step == 0:
         raise ValueError
     if step > 0:
-        if start < stop:
+        while start < stop:
             yield start
             start += step
         else:
             raise ValueError
     elif step < 0:
-        if start > stop:
+        while start > stop:
             yield start
             start += step
         else:
