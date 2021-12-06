cefb5a8b5cc5882f37b060b20c1103983bf29423
YasiaKoval <94228121+YasiaKoval@users.noreply.github.com>
2021-12-06 18:51:16 +0200

    Add files via upload

--- HT_05/task6.py
+++ HT_05/task6.py
@@ -2,4 +2,20 @@
 # P.S. Повинен вертатись генератор.
 # P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній:
 # https: // docs.python.org/3/library/stdtypes.html  # range
-# (сказали можна на наступний урок)
+def my_range(start, stop=None, step=1):
+    if stop == None:
+        start, stop = 0, step
+    if step == 0:
+        raise ValueError
+    if step > 0:
+        while True:
+            yield start
+            start += step
+    elif step < 0:
+        while True:
+            yield start
+            start += step
+
+
+for rang_num in my_range():
+    print(rang_num)
